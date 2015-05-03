ee74b288531bdb1bba9ce02f3a535fc5d2c98ca2
daveinthecave <daveman0331@gmail.com>
2015-05-03 05:56:26 -0300

    Wrote First.java Baseline

---
+++ objComp/src/objComp/util/First.java
@@ -0,0 +1,17 @@
+package objComp.util;
+
+public class First{
+	
+	private int intValue;
+	private int stringValue;
+	
+	public First(){}
+	
+	public void setIntValue(int iIn){
+		intValue = iIn;
+	}
+
+	public void setStringValue(String sIn){
+		stringValue = sIn;
+	}
+}
