a47262a9dd7e23985048f63850e2252153c8cf00
daveinthecave <daveman0331@gmail.com>
2015-05-03 13:37:41 -0300

    Second.java Initialized

---
+++ objComp/src/objComp/util/Second.java
@@ -0,0 +1,15 @@
+package objComp.util;
+
+public class Second{
+	private double doubleValue;
+	private int intValue;
+	public Second(){}
+
+	public void setIntValue(int iIn){
+		intValue = iIn;
+	}
+
+	public void setDoubleValue(double dIn){
+		doubleValue = dIn;
+	}
+}
