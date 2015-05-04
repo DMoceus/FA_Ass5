4dedc0efe5bae28ed5ba38b971f7e42bbd6d4299
daveinthecave <daveman0331@gmail.com>
2015-05-04 00:36:10 -0300

    Starting work on Reflection Code

--- objComp/src/objComp/util/PopulateObjects.java
+++ objComp/src/objComp/util/PopulateObjects.java
@@ -1,14 +1,17 @@
 package objComp.util;
 
 import java.util.HashMap;
+import objComp.fileOperations.FileProcessor;
 
 public class PopulateObjects{
 	private HashMap<First,Integer> firstMap;
 	private HashMap<Second,Integer> secondMap;
+	private FileProcessor processor;
 	
-	public PopulateObjects(){
+	public PopulateObjects(FileProcessor pIn){
 		firstMap = new HashMap<First,Integer>();
 		secondMap = new HashMap<Second,Integer>();
+		processor = pIn;
 	}
 	
 	public void addFirst(First fIn){
@@ -27,5 +30,19 @@ public class PopulateObjects{
 		else{
 			secondMap.put(sIn,1);
 		}
+	}
+	
+	public void deserObjects(){
+		String clsName;
+		Class cls;
+		Class[] signature = new Class[1];
+		String methName;
+		Method meth;
+		Object obj;
+		Object[] params = new Object[1];
+		Object result;
+
+		
+		
 	}
 }
